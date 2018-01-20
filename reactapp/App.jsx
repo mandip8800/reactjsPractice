f2cb1fe6b0b4d282d756f49e893afbbf8c9d0da2
“mandip8800” <sonypendrive11@gmail.com>
2018-01-20 17:44:13 -0600

    ReactJS Environment Set Up and First Project

---
+++ reactapp/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World!!! How are you?? How can I help you?<br />
+            This is so cool.
+         </div>
+      );
+   }
+}
+export default App;
