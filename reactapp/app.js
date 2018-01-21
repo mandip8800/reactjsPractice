f72253611eba14624e087fa3f7c09708c638b83c
“mandip8800” <sonypendrive11@gmail.com>
2018-01-20 22:19:00 -0600

    Ref Practice

--- reactapp/app.js
+++ reactapp/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import App from './AppChildEventPractice.jsx';
+import App from './AppRefPractice.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
