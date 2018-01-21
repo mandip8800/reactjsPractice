6e3f1caf0b0004f4f9375b04a2f9cb9005461828
“mandip8800” <sonypendrive11@gmail.com>
2018-01-20 22:12:58 -0600

    form practice 2

--- reactapp/app.js
+++ reactapp/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import App from './AppFormPractice.jsx';
+import App from './AppFormPractice2.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
