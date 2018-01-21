d50c93c6986a2bd9bcd3b658ef22bccdddf7aa71
“mandip8800” <sonypendrive11@gmail.com>
2018-01-20 21:05:03 -0600

    component api setState practice

--- reactapp/app.js
+++ reactapp/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import App from './AppPropsValidation.jsx';
+import App from './AppComponentApiSetState.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
