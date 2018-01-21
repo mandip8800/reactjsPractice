1baf7d5f5255420165de8858b627c06e3b048a8e
“mandip8800” <sonypendrive11@gmail.com>
2018-01-20 19:38:24 -0600

    state practice

--- reactapp/app.js
+++ reactapp/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './AppStateful.jsx';
+import App from './AppState.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
