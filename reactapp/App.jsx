0fc23666bf80bde3d3ce818fc66879dbe7d8339b
“mandip8800” <sonypendrive11@gmail.com>
2018-01-20 19:02:22 -0600

    stateless practice

--- reactapp/App.jsx
+++ reactapp/App.jsx
@@ -2,38 +2,34 @@ import React from 'react';
 
 class App extends React.Component {
    render() {
-      var i = 1;
-
-      var myStyle = {
-         fontSize: 40,
-         color: '#FF0000'
-      };
+    
 
       return (
       
          <div>
-            Hello World!!! How are you?? How can I help you?<br />
-            This is so cool.
-            <h1 style = {myStyle}>Header</h1>
-            <h2>Content</h2>
-            <p>This is the content!!</p>
-            <p>This is another content!!</p>
-            <p data-myattribute="somevalue">This is my attribute!!</p>
-
-            
-
-            <h2>{i==1?'True!' : 'False'}</h2>
-
-            <h2>{5+6}</h2>
-            
-
-          
-
-         </div>
-         
-             
+         	 <Header/>
+         	 <Content/>
+         </div>      
           
       );
    }
+}
+class Header extends React.Component{
+	render() {
+	return(
+	<div>
+		<h1>My Header</h1>
+	</div>)
+	}
+}
+class Content extends React.Component{
+	render(){
+	return(
+	<div>
+		<h2>My Content</h2>
+		<p>This is real content!!</p>
+	</div>
+	)
+	}
 }
 export default App;
