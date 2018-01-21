107fc77adf4580c980c12f028d919e257d1adc38
“mandip8800” <sonypendrive11@gmail.com>
2018-01-20 22:21:07 -0600

    keys Practice

--- reactapp/app.js
+++ reactapp/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import App from './AppRefPractice.jsx';
+import App from './AppKeysPractice.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
