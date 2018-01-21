ea5a9280a33909af2d1fba6b5f386e4c5d0f06bd
“mandip8800” <sonypendrive11@gmail.com>
2018-01-20 21:10:42 -0600

    component api force update practice

--- reactapp/app.js
+++ reactapp/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import App from './AppComponentApiSetState.jsx';
+import App from './AppComponentApiForceUpdate.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
