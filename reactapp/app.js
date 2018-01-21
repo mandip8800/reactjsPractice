d15816c82b8cf7ad44dbb3eda2429233dd0b8791
“mandip8800” <sonypendrive11@gmail.com>
2018-01-20 20:39:19 -0600

    props validation practice

--- reactapp/app.js
+++ reactapp/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import App from './AppStateAndPropPractice.jsx';
+import App from './AppPropsValidation.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
